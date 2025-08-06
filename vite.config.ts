df0b07732f7cb22e18867744f5c115250226a448
SiddhantSShende <siddhantsshende@gmail.com>
2025-08-06 16:58:29 +0530

    fix: update vite base path and force website refresh

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/RNR-WEB_DEMO/' : '/',
+  base: '/RNR-WEB_DEMO/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
